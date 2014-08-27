cf17e73df42c7a89e2bb9f00d0ce146b1d4a1f65
mbrechet <mbrechet.ext@orange.com>
2014-08-27 14:47:57 +0200

    Add serve-index
    and use serve-static now we can list directories

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ligthHttpServer
 ===============
 
-very light server that use nodejs and exprss to act as current folder as default http folder
+very light server that use nodejs and express to act as current folder as default http folder
+
+Based on the new version of express 4.x
--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 //require express and add the app
 var express  = require("express");
-var app = express();
+var serveIndex = require("serve-index");
+var serverStatic = require("serve-static");
 
+var app = express();
+var PORT = 8081;
 // remove the referecen of express in header
 app.disable("x-power-by");
 
+
 // set the static path to n-1 path
 var dirname = __dirname + "/../";
-console.info("dirname : ",dirname);
-app.use(express.static(dirname));
 
+// set index to list files lique apache
+var index = serveIndex(dirname, {'icons': true});
+// enable statics files
+var staticFiles = serverStatic(dirname);
+app.use(staticFiles);
+
+//overwride mime types
+app.use(function(req, res, next){
+ 	if(req.url.lastIndexOf(".m4s")!= -1){
+ 		res.set({"Content-Type":"video/mp4"});
+ 	}
+ 	if(req.url.lastIndexOf(".mpd")!= -1){
+ 		res.set({"Content-Type":"text/xml"});
+ 	}
+ 	// list files
+ 	index(req,res,next);
+ 	//next();
+});
+
+
+
+console.info("server started on 'localhost' port ",PORT);
 // start the server
-app.listen(8080);
+app.listen(PORT);
 
